3f3c5c7a2dca208396477d98b2892ac1c1b66435
Kevin" "Paganini <paganinik@dh-mgmt4.hpc.msoe.edu>
2023-11-29 14:10:07 -0600

    updates

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=teaching
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 command="python train.py"
 singularity exec --nv /data/sdp/senior_design_llm/containers/llm-sd.sif bash -c '
     pip install rllib ray gymnasium dm_tree pyboy pysdl2 pysdl2-dll tensorflow-probability
